7df5317a64533c6b3906074262a381a3d8b87b25
NetoHaarp <NetoHaarp@LENOVO-PCNETO>
2017-11-13 22:30:16 -0600

    Agregar archivos de proyecto.

---
+++ Pascal/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Programa
+{
+    class Pascal
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Triangulo de Pascal");
+            int primer = 1;
+            int segundo = 1;
+            int tercero = 0;
+            Console.WriteLine(primer);
+            while (tercero <= 4)
+            {
+                if (tercero == 0)
+                {
+                    Console.WriteLine(primer + "  " + primer);
+                }
+                if (tercero == 1)
+                {
+                    segundo += primer;
+                    Console.WriteLine(primer + "  " + segundo + "  " + primer);
+                }
+                if (tercero == 2)
+                {
+                    segundo += primer;
+                    Console.WriteLine(primer + "  " + segundo + "  " + segundo + "  " + primer);
+                }
+                if (tercero == 3)
+                {
+                    segundo += primer;
+
+                    Console.WriteLine(primer + "  " + segundo + " " + (tercero + tercero) + " " + segundo + "  " + primer);
+                }
+                if (tercero == 4)
+                {
+                    segundo += primer;
+                    Console.WriteLine(primer + "  " + segundo + "  " + (segundo + segundo) + (segundo + segundo) + " " + segundo + "  " + primer);
+                }
+
+
+                tercero++;
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
